6d0c1e69e53b08d8b41027fe6835b8dd4b864d80
Karan Tyagi <karantyagi9702@gmail.com>
2022-02-09 14:23:42 +0530

    Add paymentCallback

--- pages/api/payment/paynow.js
+++ pages/api/payment/paynow.js
@@ -6,7 +6,7 @@ export default async function paynow(req, res) {
 		var reqBody = JSON.parse(req.body);
 		var orderId = "RSGI" + Math.floor(Math.random(6) * 1000000);
 
-		var callbackUrl = "https://spysr-project.vercel.app/dashboard";
+		var callbackUrl = "https://spysr-project.vercel.app/api/payment/paymentCallback";
 		var userInfo = {
 			custId: reqBody.custId, // CLIENT CUSTOMER ID
 			mobile: reqBody.mobile,
